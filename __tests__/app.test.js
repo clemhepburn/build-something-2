ad435e0136622a40223ba647e4bb85ed64f5f279
Clem Hepburn <cooper.hepburn@pcc.edu>
2021-06-22 12:15:09 -0700

    create failing POST test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,4 +7,16 @@ describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  it('creates a cup of coffee via POST', async () => {
+    const res = await request(app)
+      .post('api/v1/coffees')
+      .send({
+        quantity: 1
+      });
+    expect(res.body).toEqual({
+      id: 1,
+      quantity: 1
+    });
+  });
 });
